13ed46857ffdedb007a06799b115fb59b9e93e5d
h95fit <h95fit@gmail.com>
2025-04-21 18:21:06 +0900

    FastAPI, React 초기 환경설정 및 디렉토리 구성

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from app!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from routers import example
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(example.router)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return "<h1>Hello, FastAPI!</h1>"
